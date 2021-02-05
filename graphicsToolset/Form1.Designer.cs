69089f55d3e00129ae01e3ce24decc6d61e9990f
Rick Fleischer <rcfleischer@gmail.com>
2021-02-05 17:01:09 -0500

    displaying and convolving

--- graphicsToolset/Form1.Designer.cs
+++ graphicsToolset/Form1.Designer.cs
@@ -37,6 +37,9 @@ namespace graphicsToolset
             this.toolStatus = new System.Windows.Forms.TextBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.openFile = new System.Windows.Forms.Button();
+            this.process01 = new System.Windows.Forms.Button();
+            this.process02 = new System.Windows.Forms.Button();
+            this.process03 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultBox01)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultBox02)).BeginInit();
@@ -48,9 +51,10 @@ namespace graphicsToolset
             this.inputPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.inputPictureBox.Location = new System.Drawing.Point(12, 121);
             this.inputPictureBox.Name = "inputPictureBox";
-            this.inputPictureBox.Size = new System.Drawing.Size(199, 182);
+            this.inputPictureBox.Size = new System.Drawing.Size(256, 256);
             this.inputPictureBox.TabIndex = 0;
             this.inputPictureBox.TabStop = false;
+            this.inputPictureBox.Click += new System.EventHandler(this.inputPictureBox_Click);
             // 
             // textBox1
             // 
@@ -66,61 +70,97 @@ namespace graphicsToolset
             // resultBox01
             // 
             this.resultBox01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.resultBox01.Location = new System.Drawing.Point(233, 121);
+            this.resultBox01.Location = new System.Drawing.Point(300, 121);
             this.resultBox01.Name = "resultBox01";
-            this.resultBox01.Size = new System.Drawing.Size(132, 125);
+            this.resultBox01.Size = new System.Drawing.Size(256, 256);
             this.resultBox01.TabIndex = 2;
             this.resultBox01.TabStop = false;
+            this.resultBox01.Click += new System.EventHandler(this.resultBox01_Click);
             // 
             // resultBox02
             // 
             this.resultBox02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.resultBox02.Location = new System.Drawing.Point(382, 121);
+            this.resultBox02.Location = new System.Drawing.Point(576, 121);
             this.resultBox02.Name = "resultBox02";
-            this.resultBox02.Size = new System.Drawing.Size(132, 125);
+            this.resultBox02.Size = new System.Drawing.Size(256, 256);
             this.resultBox02.TabIndex = 3;
             this.resultBox02.TabStop = false;
+            this.resultBox02.Click += new System.EventHandler(this.resultBox02_Click);
             // 
             // resultBox03
             // 
             this.resultBox03.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.resultBox03.Location = new System.Drawing.Point(530, 121);
+            this.resultBox03.Location = new System.Drawing.Point(510, 445);
             this.resultBox03.Name = "resultBox03";
-            this.resultBox03.Size = new System.Drawing.Size(132, 125);
+            this.resultBox03.Size = new System.Drawing.Size(72, 67);
             this.resultBox03.TabIndex = 4;
             this.resultBox03.TabStop = false;
             // 
             // toolStatus
             // 
-            this.toolStatus.Location = new System.Drawing.Point(12, 321);
+            this.toolStatus.Location = new System.Drawing.Point(25, 392);
             this.toolStatus.Multiline = true;
             this.toolStatus.Name = "toolStatus";
-            this.toolStatus.Size = new System.Drawing.Size(199, 107);
+            this.toolStatus.Size = new System.Drawing.Size(436, 107);
             this.toolStatus.TabIndex = 5;
             // 
             // openFileDialog
             // 
-            this.openFileDialog.FileName = "openFileDialog";
-            this.openFileDialog.InitialDirectory = "c:\\";
-            this.openFileDialog.Tag = "whumEyeGoinDo";
-            this.openFileDialog.Title = "Does this show up?";
-            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            this.openFileDialog.InitialDirectory = "C:\\Users\\merle\\Desktop\\icons\\big";
+            this.openFileDialog.Tag = "do I need a tag?";
+            this.openFileDialog.Title = "Select the file to annalyze";
             // 
             // openFile
             // 
-            this.openFile.Location = new System.Drawing.Point(509, 43);
+            this.openFile.Location = new System.Drawing.Point(454, 1);
             this.openFile.Name = "openFile";
             this.openFile.Size = new System.Drawing.Size(68, 49);
             this.openFile.TabIndex = 7;
-            this.openFile.Text = "Open File. I hope.";
+            this.openFile.Text = "Open File";
             this.openFile.UseVisualStyleBackColor = true;
             this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // process01
+            // 
+            this.process01.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.process01.Location = new System.Drawing.Point(457, 52);
+            this.process01.Name = "process01";
+            this.process01.Size = new System.Drawing.Size(68, 49);
+            this.process01.TabIndex = 8;
+            this.process01.Text = "1";
+            this.process01.UseVisualStyleBackColor = true;
+            this.process01.Click += new System.EventHandler(this.straightCopy);
+            // 
+            // process02
+            // 
+            this.process02.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.process02.Location = new System.Drawing.Point(531, 53);
+            this.process02.Name = "process02";
+            this.process02.Size = new System.Drawing.Size(68, 49);
+            this.process02.TabIndex = 9;
+            this.process02.Text = "2";
+            this.process02.UseVisualStyleBackColor = true;
+            this.process02.Click += new System.EventHandler(this.markBackslash);
+            // 
+            // process03
+            // 
+            this.process03.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.process03.Location = new System.Drawing.Point(605, 53);
+            this.process03.Name = "process03";
+            this.process03.Size = new System.Drawing.Size(68, 49);
+            this.process03.TabIndex = 10;
+            this.process03.Text = "3";
+            this.process03.UseVisualStyleBackColor = true;
+            this.process03.Click += new System.EventHandler(this.explodeSlash);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(843, 534);
+            this.Controls.Add(this.process03);
+            this.Controls.Add(this.process02);
+            this.Controls.Add(this.process01);
             this.Controls.Add(this.openFile);
             this.Controls.Add(this.toolStatus);
             this.Controls.Add(this.resultBox03);
@@ -149,6 +189,9 @@ namespace graphicsToolset
         private System.Windows.Forms.TextBox toolStatus;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button process01;
+        private System.Windows.Forms.Button process02;
+        private System.Windows.Forms.Button process03;
     }
 }
 
--- graphicsToolset/Form1.cs
+++ graphicsToolset/Form1.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace graphicsToolset
 {
     public partial class Form1 : Form
     {
+        delegate Color munger(Bitmap inmap, int x, int y, int width, int height);
         StringBuilder stringBuilder = new StringBuilder();
         string openPath;
         public Form1()
@@ -24,29 +21,226 @@ namespace graphicsToolset
             toolStatus.Text = stringBuilder.ToString();
             return this;
         }
-        protected override void OnFormClosing(FormClosingEventArgs e)
-        {
-            openPath = "mooga";
-        }
-        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
         private void openFile_Click(object sender, EventArgs e)
         {
+            openFileDialog.InitialDirectory = @"C:\Users\merle\Desktop\icons\big";
             openFileDialog.ShowDialog();
             addMessage($"Using file {openFileDialog.FileName}");
-            openPath = @"C:\Users\merle\Desktop\icons\big\me.png";// openFileDialog.FileName;
-            using (Graphics windowGraphics = inputPictureBox.CreateGraphics())
+            openPath = openFileDialog.FileName; // @"C:\Users\merle\Desktop\icons\big\me.png";// 
+        }
+        private static void mungEachPixel(Bitmap inmap, Bitmap outmap, munger munger)
+        {
+            int height = inmap.Height;
+            var width = inmap.Width;
+            foreach(int x in Enumerable.Range(0, width - 1))
+            {
+                foreach(int y in Enumerable.Range(0, height - 1))
+                {
+                    outmap.SetPixel(x, y, munger(inmap, x, y, width, height));
+                }
+            }
+        }
+        private Bitmap extremes(Bitmap map, out Color maxColor, out int xMax, out int yMax, out Color minColor, out int xMin, out int yMin)
+        {
+            bigBitmap big = new bigBitmap(map);
+            Bitmap shitsAndGrins = new Bitmap(map);
+            xMax = int.MinValue;
+            yMax = int.MinValue;
+            xMin = int.MaxValue;
+            yMin = int.MaxValue;
+            int height = map.Height;
+            var width = map.Width;
+            double minimum = double.MaxValue;
+            double maximum = double.MinValue;
+            bigColor bigMaxColor=new bigColor();
+            bigColor bigMinColor=new bigColor();
+            foreach(int x in Enumerable.Range(0, width - 1))
+                foreach(int y in Enumerable.Range(0, height - 1))
+                {
+                    bigColor bigColor = big.data[x,y];
+                    double magnitude = bigColor.magnitude;
+                    if(magnitude > maximum && magnitude < 250)
+                    {
+                        maximum = magnitude;
+                        bigMaxColor = bigColor;
+                        xMax = x;
+                        yMax = y;
+                    }
+                    if(magnitude < minimum && magnitude > 6)
+                    {
+                        minimum = magnitude;
+                        bigMinColor = bigColor;
+                        xMin = x;
+                        yMin = y;
+                    }
+                    shitsAndGrins.SetPixel(x, y, bigMaxColor.toColor());
+                }
+            maxColor = bigMaxColor.toColor();
+            minColor = bigMinColor.toColor();
+            addMessage($"Max {maxColor} at ({xMax},{yMax}); min {minColor} ({xMin},{yMin})");
+            return shitsAndGrins;
+        }
+        static readonly double[,] scale = new double[3,3]
+        {
+            {-1.0, -1.0, -1.0},
+            {-1.0, 8.0, -1.0},
+            {-1.0, -1.0, -1.0}
+        };
+        class bigColor
+        {
+            public double R,G,B,A;
+            public bigColor(double alpha = 1, double red = 0, double green = 0, double blue = 0)
+            {
+                A = alpha;
+                B = blue;
+                G = green;
+                R = red;
+            }
+            public static bigColor operator +(bigColor to, bigColor addend) =>
+                new bigColor(Math.Max(addend.A, to.A), addend.R + to.R, addend.G + to.G, addend.B + to.B);
+            public static bigColor operator *(bigColor to, double scale) =>
+                new bigColor(to.A, to.R * scale, scale * to.G, scale * to.B);
+            public bigColor(Color from) : this(from.A, from.R, from.G, from.B) { }
+            public double magnitude => R + G + B;
+            public Color toColor() =>
+                Color.FromArgb(
+                    (int)Math.Min(255, Math.Max(0, A)),
+                    (int)Math.Min(255, Math.Max(0, R)),
+                    (int)Math.Min(255, Math.Max(0, G)),
+                    (int)Math.Min(255, Math.Max(0, B)));
+        }
+        class bigBitmap
+        {
+            int width=0, height=0;
+            public bigColor[,] data;
+            public bigBitmap(Bitmap from)
+            {
+                width = from.Width;
+                height = from.Height;
+                data = new bigColor[width, height];
+                foreach(int x in Enumerable.Range(0, width - 1))
+                    foreach(int y in Enumerable.Range(0, height - 1))
+                    {
+                        data[x, y] = new bigColor(from.GetPixel(x, y));
+                    }
+            }
+            public Bitmap getBitmap ()
+            {
+                Bitmap working = new Bitmap(width,height);
+                foreach(int x in Enumerable.Range(0, width - 1))
+                    foreach(int y in Enumerable.Range(0, height - 1))
+                        working.SetPixel(x, y, data[x, y].toColor());
+                return working;
+            }
+        }
+        private static void convolveBitmap(Bitmap bitmap)
+        {
+            Bitmap copy = new Bitmap(bitmap);
+            foreach(int x in Enumerable.Range(0, bitmap.Width - 1))
+                foreach(int y in Enumerable.Range(0, bitmap.Height - 1))
+                {
+                    bigColor accum = new bigColor();
+                    if(x > 0 && y > 0)
+                    {
+                        accum += new bigColor(copy.GetPixel(x - 1, y - 1)) * scale[0, 0];
+                        accum += new bigColor(copy.GetPixel(x, y - 1)) * scale[1, 0];
+                        accum += new bigColor(copy.GetPixel(x + 1, y - 1)) * scale[2, 0];
+                        accum += new bigColor(copy.GetPixel(x - 1, y)) * scale[0, 1];
+                        accum += new bigColor(copy.GetPixel(x, y)) * scale[1, 1];
+                        accum += new bigColor(copy.GetPixel(x + 1, y)) * scale[2, 1];
+                        accum += new bigColor(copy.GetPixel(x - 1, y + 1)) * scale[0, 2];
+                        accum += new bigColor(copy.GetPixel(x, y + 1)) * scale[1, 2];
+                        accum += new bigColor(copy.GetPixel(x + 1, y + 1)) * scale[2, 2];
+                        bitmap.SetPixel(x, y, accum.toColor());
+                    }
+                }
+        }
+        private void convolveAll()
+        {
+            using(Graphics windowGraphics = resultBox02.CreateGraphics())
             {
                 Point point = new Point(0, 0);// inputPictureBox.Location;
                 Size size = inputPictureBox.Size;
                 Rectangle rectangle = new Rectangle(point, size);
-                Image image = Image.FromFile(openPath);
-                windowGraphics.CopyFromScreen(new Point(0, 0), point, size);
-                windowGraphics.DrawImage(image, rectangle);
+                Bitmap bitmap = (Bitmap)Image.FromFile(openPath);
+                convolveBitmap(bitmap);
+                //windowGraphics.CopyFromScreen(new Point(0, 0), point, size);
+                windowGraphics.DrawImage(bitmap, rectangle);
             }
+        }
+        static bool interested(int x, int y, int width, int height)
+        {
+            int wDivision = width / 3;
+            int hDivision = height / 3;
+            if(x < wDivision || x > 2 * wDivision) return false;
+            if(y < hDivision || y > 2 * hDivision) return false;
+            return (x == y);
+        }
+        private static Color invertBackslash(Bitmap inmap, int x, int y, int width, int height)
+        {
+            Color pixel = inmap.GetPixel(x, y);
+            if(interested(x, y, width, height))
+            {
+                pixel = Color.FromArgb(alpha: 255, red: 255 - pixel.R, green: 255 - pixel.G, blue: 255 - pixel.B);
+            }
+            else
+                pixel = Color.FromArgb(alpha: 255, red: pixel.R, green: pixel.G, blue: pixel.B);
+            return pixel;
+        }
+        private void straightCopy(object sender, EventArgs e)
+        {
+            using(Graphics windowGraphics = inputPictureBox.CreateGraphics())
+            {
+                Point point = new Point(0, 0);
+                Size size = inputPictureBox.Size;
+                Rectangle pictureBox = new Rectangle(point, size);
+                Bitmap bitmap = (Bitmap)Image.FromFile(openPath);
+                Rectangle imageBox = new Rectangle(point, bitmap.Size);
+                windowGraphics.DrawImage(bitmap, imageBox); // pictureBox
+            }
+        }
+        private void markBackslash(object sender, EventArgs e)
+        {
+            using(Graphics windowGraphics = resultBox01.CreateGraphics())
+            {
+                Point point = new Point(0, 0);// inputPictureBox.Location;
+                Size size = inputPictureBox.Size;
+                Rectangle rectangle = new Rectangle(point, size);
+                Bitmap bitmap = (Bitmap)Image.FromFile(openPath);
+                bitmap = extremes(bitmap, out Color maxColor, out int xMax, out int yMax, out Color miinColor, out int xMin, out int yMin);
+                //mungEachPixel(bitmap, bitmap, invertBackslash);
+                //windowGraphics.CopyFromScreen(new Point(0, 0), point, size);
+                windowGraphics.DrawImage(bitmap, rectangle);
+            }
+        }
+        private void explodeSlash(object sender, EventArgs e)
+        {
+            convolveAll();
+            //using(Graphics windowGraphics = resultBox02.CreateGraphics())
+            //{
+            //    Point point = new Point(0, 0);// inputPictureBox.Location;
+            //    Size size = inputPictureBox.Size;
+            //    Rectangle rectangle = new Rectangle(point, size);
+            //    Bitmap inmap = (Bitmap)Image.FromFile(openPath);
+            //    Bitmap outmap = new Bitmap(size.Width,size.Height);
+            //    mungEachPixel(inmap, outmap, invertBackslash);
+            //    windowGraphics.DrawImage(outmap, rectangle);
+            //}
+        }
+
+        private void inputPictureBox_Click(object sender, EventArgs e)
+        {
+            straightCopy(sender, e);
+        }
+
+        private void resultBox01_Click(object sender, EventArgs e)
+        {
+            markBackslash(sender, e);
+        }
+
+        private void resultBox02_Click(object sender, EventArgs e)
+        {
+            explodeSlash(sender, e);
         }
     }
 }
